8461a1b87233b15663392db0276cb2fbca1cd303
mahdiyaroraei <moa15928@gmail.com>
2017-05-01 21:00:39 +0430

    Close keyboard

--- PeriodTracker/LogPeriodViewController.swift
+++ PeriodTracker/LogPeriodViewController.swift
@@ -16,9 +16,23 @@ class LogPeriodViewController: UIViewController {
         super.viewDidLoad()
 
         navItem.leftBarButtonItem = UIBarButtonItem(title: "done", style: .done, target: self, action: #selector(done))
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+        
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
     
     func done() {
+        dismissKeyboard()
         self.dismiss(animated: true, completion: nil)
     }
 
