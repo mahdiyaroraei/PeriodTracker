aba98472fc59302134fbffea5c96397bcb2ba5ca
mahdiyaroraei <moa15928@gmail.com>
2017-05-01 16:42:36 +0430

    period time realm object added

---
+++ PeriodTracker/PeriodTime.swift
@@ -0,0 +1,19 @@
+//
+//  PeriodTime.swift
+//  Period Tracker
+//
+//  Created by Mahdiar  on 5/1/17.
+//  Copyright © 2017 Mahdiar . All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class PeriodTime: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
